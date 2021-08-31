df1f7a24146ea0e9b60501de32dfb53f2427e45e
rakeshdeka <rakeshdekaa21@gmail.com>
2021-08-31 09:17:02 +0530

    new files

---
+++ src/com/rakesh/TypeCasting.java
@@ -0,0 +1,42 @@
+package com.rakesh;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+//        int
+//        short
+//        long
+//        float
+//        double
+//        byte
+//        char;
+
+
+//        WideningTypeCasting(byte -> short -> char -> int -> long -> float -> double  )
+//        byte a= 2;
+//        short b= a;
+//        char c='b';
+//        int d= c;
+//        long e= d;
+//        float f= e;
+//        double g = f;
+//        System.out.println("before conversion byte value"+a);
+//        System.out.println("after conversion, short value" +b);
+//        System.out.println("after conversion, char value" +c);
+//        System.out.println("after conversion, int value" +d);
+//        System.out.println("after conversion, long value" +e);
+//        System.out.println("after conversion, float value" +f);
+//        System.out.println("after conversion, double value" +g);
+
+
+//        Narrowing Type Casting(double -> float -> long -> int -> char -> short -> byte )
+//        double a= 166.564;
+//        short b= (short) a; // some parts lost
+//        System.out.println(b);
+        char x ='a';
+        short u = (short)'a';
+        System.out.println(u);
+
+
+
+    }
+}

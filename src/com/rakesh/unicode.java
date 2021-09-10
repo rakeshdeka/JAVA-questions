7555b7e63c64f3c57bcfda99ffac9e9853878ffa
rakeshdeka <rakeshdekaa21@gmail.com>
2021-09-10 08:36:57 +0530

    added new files

---
+++ src/com/rakesh/unicode.java
@@ -0,0 +1,15 @@
+package com.rakesh;
+
+public class unicode {
+    public static void main(String[] args) {
+        char ch ='\u0041';
+        char b = ch;
+        System.out.println(b);
+        char x = ';';
+        int y =x;
+        System.out.println(y);
+        int t= 2476;
+        char r = (char) t;
+        System.out.println(r);
+    }
+}

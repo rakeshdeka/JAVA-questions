8f2b3d875e9316432a37739fa73b68a5b61ca0bf
rakeshdeka <rakeshdekaa21@gmail.com>
2021-12-12 11:14:59 +0530

    new

---
+++ src/com/rakesh/Pattern.java
@@ -0,0 +1,19 @@
+package com.rakesh;
+
+public class Pattern {
+    //output:     *
+    //            * *
+    //            * * *
+    //            * * * *
+
+
+    public static void main(String[] args) {
+
+        for (int row = 1; row <= 4; row++) {
+            for (int col = 1; col <= row ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+    }
+}
